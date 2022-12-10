60a2c5cc81502d6e5aff430c3a57b2372511c660
Soomin Lim <augustth511@gmail.com>
2022-12-10 21:15:26 +0900

    test(baseball): 컴퓨터의 수에서 해당 위치에 있는 숫자를 반환하는 numberOf() 메서드 테스트 코드 작성

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -1,12 +1,13 @@
 package baseball.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ComputerTest {
 
@@ -25,6 +26,15 @@ class ComputerTest {
 
         int result = computer.match(player);
 
-        Assertions.assertThat(result).isEqualTo(expected);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("getNumberOf 메서드는 컴퓨터의 수에서 해당 위치에 있는 숫자를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"0,1", "1,2", "2,3"})
+    void numberOf(int index, int expected) {
+        int result = computer.numberOf(index);
+
+        assertThat(result).isEqualTo(expected);
     }
 }
