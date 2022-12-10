3ebd531f76b65e91abba98ee75ecd8822fabe4bb
Soomin Lim <augustth511@gmail.com>
2022-12-10 20:59:37 +0900

    test(baseball): 컴퓨터의 수와 플레이어의 수 둘 다에 포함되어있는 숫자의 개수를 반환하는 match() 메서드 테스트 코드 작성

---
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -0,0 +1,31 @@
+package baseball.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+class ComputerTest {
+
+    Computer computer;
+
+    @BeforeEach
+    void setUp() {
+        computer = new Computer(List.of(1,2,3));
+    }
+
+    @DisplayName("match 메서드는 컴퓨터의 수와 플레이어의 수를 비교해 둘 다에 포함되어있는 숫자의 개수를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"1,2,3,3", "1,2,5,2", "1,6,7,1", "7,8,9,0"})
+    void match(int number1, int number2, int number3, int expected) {
+        Player player = new Player(List.of(number1, number2, number3));
+
+        int result = computer.match(player);
+
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
